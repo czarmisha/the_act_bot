256ff6fd304903b12ea4c4d0419e946a2115e28c
mdergachyov <m.dergachyov@bringo.uz>
2024-06-05 14:06:21 +0500

    migration

--- the_act_bot/src/database/enums.py
+++ the_act_bot/src/database/enums.py
@@ -17,3 +17,17 @@ class DiscountTypeEnums(str, enum.Enum):
     FIXED = "fixed"
     PERCENT = "percent"
     PROMO = "promo"
+
+
+class OrderStatusEnums(str, enum.Enum):
+    NEW = "new"
+    PAYED = "payed"
+    CANCELED = "canceled"
+    COMPLETED = "completed"
+
+
+class PaymentMethodEnums(str, enum.Enum):
+    CASH = "cash"
+    CLICK = "click"
+    PAYME = "payme"
+    UZUM = "uzum"
--- the_act_bot/src/database/models/order.py
+++ the_act_bot/src/database/models/order.py
@@ -2,6 +2,7 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 import the_act_bot.src.database.models as models
+import the_act_bot.src.database.enums as enums
 
 
 class Order(models.BaseModel):
@@ -9,6 +10,8 @@ class Order(models.BaseModel):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    payment_method: Mapped[enums.PaymentMethodEnums] = mapped_column(nullable=False, default=enums.PaymentMethodEnums.CASH)
+    status: Mapped[enums.OrderStatusEnums] = mapped_column(nullable=False, default=enums.OrderStatusEnums.NEW)
 
     user: Mapped["models.User"] = relationship("User", back_populates="orders", foreign_keys=[user_id])
     items: Mapped[list["OrderItem"]] = relationship("OrderItem", back_populates="order")
